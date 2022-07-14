cdb35ff68aa2a4c628960af1cf87c9b8f2ac28d7
Oleksandr Kravets <kravetsalexandrv@gmail.com>
2022-07-14 13:11:24 +0300

    fix deploy 2

--- src/main/java/com/example/optika/config/WebSecurityConfig.java
+++ src/main/java/com/example/optika/config/WebSecurityConfig.java
@@ -24,30 +24,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/", "/registration", "/uploads/**", "/static/**", "/products", "/orders/**", "/thx").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .formLogin()
-            .loginPage("/login")
-            .permitAll()
-            .and()
-            .logout()
-            .permitAll();
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/registration", "/uploads/**", "/static/**", "/products", "/orders/**", "/thx").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
 
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService)
-            .passwordEncoder(passwordEncoder());
+                .passwordEncoder(passwordEncoder());
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
                 .antMatchers("/images/**")
+                .antMatchers("/img/**")
                 .antMatchers("/uploads/**")
         ;
     }
